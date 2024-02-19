f1f6b014761de5cb30af216ad5ec3c6d8192d27c
Simon Okwori <simon.okwori@gmail.com>
2024-02-19 01:24:16 -0600

    Format cljs ns

--- src/cljs/com/pringwa/optiliv/xhr/send/del.cljs
+++ src/cljs/com/pringwa/optiliv/xhr/send/del.cljs
@@ -4,18 +4,19 @@
             day8.re-frame.http-fx
             [kee-frame.core :as k]))
 
-(k/reg-event-fx :logout
-                (fn [{:keys [db]} _]
-                  {:http-xhrio {:method :delete
-                                :uri (api-url "/logout")
-                                :format          (ajax/text-request-format)
-                                :response-format (ajax/text-response-format)
-                                :on-success [:logout-success]
-                                :on-failure [:logout-failure]}
-                   :db (-> db
-                           (assoc-in [:xhr :logout] {:in-flight? true})
-                           (dissoc :current-user)
-                           ;; prevent login page from redirecting home:
-                           (update :xhr dissoc :login)
-                           (update :xhr dissoc :current-user))}))
+(k/reg-event-fx
+  :logout
+  (fn [{:keys [db]} _]
+    {:http-xhrio {:method          :delete
+                  :uri             (api-url "/logout")
+                  :format          (ajax/text-request-format)
+                  :response-format (ajax/text-response-format)
+                  :on-success      [:logout-success]
+                  :on-failure      [:logout-failure]}
+     :db         (-> db
+                     (assoc-in [:xhr :logout] {:in-flight? true})
+                     (dissoc :current-user)
+                     ;; prevent login page from redirecting home:
+                     (update :xhr dissoc :login)
+                     (update :xhr dissoc :current-user))}))
 
--- src/cljs/com/pringwa/optiliv/xhr/send/post.cljs
+++ src/cljs/com/pringwa/optiliv/xhr/send/post.cljs
@@ -8,19 +8,20 @@
   ([path form-data on-success on-failure]
    (post-config path form-data on-success on-failure nil))
   ([path form-data on-success on-failure overrides]
-   (merge {:method :post
-           :uri (api-url path)
-           :body form-data
-           :format :multipart
+   (merge {:method          :post
+           :uri             (api-url path)
+           :body            form-data
+           :format          :multipart
            :response-format (ajax/transit-response-format)
-           :on-success on-success
-           :on-failure on-failure}
+           :on-success      on-success
+           :on-failure      on-failure}
           overrides)))
 
-(k/reg-event-fx :login
-                (fn [cofx [form-data]]
-                  {:http-xhrio (post-config "/login" form-data
-                                            [:login-success form-data]
-                                            [:login-failure])
-                   :db (assoc-in (:db cofx) [:xhr :login] {:in-flight? true})}))
+(k/reg-event-fx
+  :login
+  (fn [cofx [form-data]]
+    {:http-xhrio (post-config "/login" form-data
+                              [:login-success form-data]
+                              [:login-failure])
+     :db         (assoc-in (:db cofx) [:xhr :login] {:in-flight? true})}))
 
