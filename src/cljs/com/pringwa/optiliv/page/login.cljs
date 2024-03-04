1b77624084cb3a3dfd45afd4d55f45609281e204
Simon Okwori <simon.okwori@gmail.com>
2024-03-03 19:16:40 -0600

    Format login fn and evt

--- src/cljs/com/pringwa/optiliv/page/login.cljs
+++ src/cljs/com/pringwa/optiliv/page/login.cljs
@@ -18,45 +18,47 @@
     "Trouble logging in?"]])
 
 (defn- login-form []
-  (r/with-let [xhr-status (rf/subscribe [::data/xhr :login])]
-              [:form {:method "post"
-                      :action (api-url "/login")
-                      :on-submit #(do
-                                    (.preventDefault %)
-                                    (rf/dispatch [:login (js/FormData.
-                                                           (.-target %))]))}
-               [:div.field
-                [:label.label "Your Email"]
-                [:div.control
-                 [:input.input {:type "email", :name "email" :placeholder "email@example.com"}]]]
-               [:div.field
-                [:label.label "Your Password"]
-                [:div.control
-                 [:input.input {:type "password", :name "password" :placeholder "********"}]]]
-               [:hr]
-               [:div.field.is-grouped
-                [:div.control
-                 [:input.button.is-primary {:type "submit", :value "Login"}]]
-                [forgot-password-link]]
-               [xhrs/xhr-status :login @xhr-status]]))
+  (r/with-let
+    [xhr-status (rf/subscribe [::data/xhr :login])]
+    [:form {:method    "post"
+            :action    (api-url "/login")
+            :on-submit #(do
+                          (.preventDefault %)
+                          (rf/dispatch [:login (js/FormData.
+                                                 (.-target %))]))}
+     [:div.field
+      [:label.label "Your Email"]
+      [:div.control
+       [:input.input {:type "email", :name "email" :placeholder "email@example.com"}]]]
+     [:div.field
+      [:label.label "Your Password"]
+      [:div.control
+       [:input.input {:type "password", :name "password" :placeholder "********"}]]]
+     [:hr]
+     [:div.field.is-grouped
+      [:div.control
+       [:input.button.is-primary {:type "submit", :value "Login"}]]
+      [forgot-password-link]]
+     [xhrs/xhr-status :login @xhr-status]]))
 
 (defn login-page []
-  (r/with-let [xhr-status (rf/subscribe [::data/xhr :current-user])]
-              [:<>
-               [layout/modal "login"
-                [:div.columns.is-centered
-                 [:div.column.is-two-fifths
-                  [:div.card.has-card-header-background
-                   [:header.card-header.has-background-primary
-                    [:p.card-header-title
-                     [:span.icon [:i.mdi.mdi-lock.default]]
-                     "Login"]]
-                   [:div.card-content
-                    (let [{:keys [in-flight? success?]} @xhr-status]
-                      (cond
-                        in-flight? "Loading… ⏳"
-                        success? (rf/dispatch [:navigate-to-authenticated-home])
-                        :else [:<>
-                               [login-form]
-                               [:br]]))]]]]]
-               [forgot-password-modal]]))
+  (r/with-let
+    [xhr-status (rf/subscribe [::data/xhr :current-user])]
+    [:<>
+     [layout/modal "login"
+      [:div.columns.is-centered
+       [:div.column.is-two-fifths
+        [:div.card.has-card-header-background
+         [:header.card-header.has-background-primary
+          [:p.card-header-title
+           [:span.icon [:i.mdi.mdi-lock.default]]
+           "Login"]]
+         [:div.card-content
+          (let [{:keys [in-flight? success?]} @xhr-status]
+            (cond
+              in-flight? "Loading… ⏳"
+              success? (rf/dispatch [:navigate-to-authenticated-home])
+              :else [:<>
+                     [login-form]
+                     [:br]]))]]]]]
+     [forgot-password-modal]]))
