08eb2c3f4e0aec71965b38fb2a62e86d1b3eecd6
Simon Okwori <simon.okwori@gmail.com>
2024-04-08 04:18:41 -0500

    Property tbl migration

---
+++ resources/migrations/20240408090409-add-property-table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS
+    property
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price FLOAT,
+    address VARCHAR(250),
+    adjusted_by INT,
+    user_id INT,
+    description VARCHAR(500),
+    city_area_id INT,
+    property_type_id INT
+);
