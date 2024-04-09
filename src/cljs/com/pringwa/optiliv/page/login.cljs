1c75495d244b935e5441a1c2563e760ad5a3fd59
Simon Okwori <simon.okwori@gmail.com>
2024-04-09 09:51:18 -0500

    Stash login page!

--- src/cljs/com/pringwa/optiliv/page/login.cljs
+++ src/cljs/com/pringwa/optiliv/page/login.cljs
@@ -41,7 +41,7 @@
       [forgot-password-link]]
      [xhrs/xhr-status :login @xhr-status]]))
 
-(defn login-page2 []
+(defn login-page []
   (r/with-let
     [xhr-status (rf/subscribe [::data/xhr :current-user])]
     [:<>
@@ -64,114 +64,133 @@
      [forgot-password-modal]]
     ))
 
-(defn login-page []
+(defn login-page2 []
   (r/with-let
     [xhr-status (rf/subscribe [::data/xhr :current-user])]
     [:<>
+
      ;<div style="background: url(/img/mountains.png)"></div>
      [:section.hero.is-info.is-fullheight.has-background-image
+      {:style {:background "url(/img/mountains.png)"}}
       [:ul.nav
-       [:li {:onclick "showLogin()"} "Log In"]
-       [:li {:onclick "showSignup()"} "Sign up"]
-       [:li {:onclick "showForgotPassword()"} "Forgot password"]
-       [:li {:onclick "showSubscribe()"} "Subscribe"]
-       [:li {:onclick "showContactUs()"} "Contact us"]]
+       [:li  "Log In"]
+       [:li  "Sign up"]
+       [:li  "Forgot password"]
+       [:li  "Subscribe"]
+       [:li  "Contact us"]]
       [:div.wrapper
        [:div.rec-prism
         [:div.face.face-top
          [:div.content
           [:h2 "Subscribe"]
           [:small "Enter your email so we can send you the latest updates!"]
-          [:form {:onsubmit "event.preventDefault()"}
+          [:form
            [:div.field-wrapper
-            [:input {:type "text" :name "email" :placeholder "email" :required "true"}]
+            [:input {:type "text" :name "email" :placeholder "email" :required true}]
             [:label "e-mail"]]
            [:div.field-wrapper
             [:input {:type "submit"}]]]]]
         [:div.face.face-front
          [:div.content
           [:h2 "Log in"]
-          [:form {:onsubmit "event.preventDefault()"}
-           [:div.field
-            [:label.label {:for ""} "Email"]
-            [:div.control.has-icons-left
-             [:input.input {:type "email" :placeholder "e.g. bobsmith@gmail.com" :required "true"}]
-             [:span.icon.is-small.is-left
-              [:i.mdi.mdi-email-box]]]]
-           [:div.field
-            [:label.label "Password"]
-            [:div.control.has-icons-left
-             [:input.input {:type "password" :placeholder "*******" :required "true"}]
-             [:span.icon.is-small.is-left
-              [:i.mdi.mdi-lock]]]]
-           [:div.field
-            [:label.checkbox
-             [:input {:type "checkbox"}] "Remember me"]
-            [:span.psw {:onclick "showForgotPassword()" :style "margin-top: 5px; margin-bottom: 0px; text-align: left;"} "Forgot Password?"]]
-           [:div.divider {:style "display: flex; justify-content: center; align-items: center;"} "OR"]
-           [:div.social-buttons
-            [:button.button.is-fullwidth {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
-             [:span.icon
-              [:i.mdi.mdi-google]]
-             [:span "Log in with Google"]]
-            [:button.button.is-fullwidth {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
-             [:span.icon
-              [:i.mdi.mdi-facebook]]
-             [:span "Log in with Facebook"]]
-            [:button.button.is-fullwidth {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
-             [:span.icon
-              [:i.mdi.mdi-apple]]
-             [:span "Log in with Apple"]]]
-           [:div.field-wrapper
-            [:input {:type "submit" :style "margin-top:-20px"}]]
-           [:span.signup {:onclick "showSignup()" :style "margin-top:5px"} "Not a user?  Sign up"]]]]
+          ;[:form
+          ; [:div.field
+          ;  [:label.label {:for ""} "Email"]
+          ;  [:div.control.has-icons-left
+          ;   [:input.input {:type "email" :placeholder "e.g. bobsmith@gmail.com" :required true}]
+          ;   [:span.icon.is-small.is-left
+          ;    [:i.mdi.mdi-email-box]]]]
+          ; [:div.field
+          ;  [:label.label "Password"]
+          ;  [:div.control.has-icons-left
+          ;   [:input.input {:type "password" :placeholder "*******" :required true}]
+          ;   [:span.icon.is-small.is-left
+          ;    [:i.mdi.mdi-lock]]]]
+          ; [:div.field
+          ;  [:label.checkbox
+          ;   [:input {:type "checkbox"}] "Remember me"]
+          ;  [:span.psw
+          ;   {:style "margin-top: 5px; margin-bottom: 0px; text-align: left;"}
+             ;"Forgot Password?"]]
+           ;[:div.divider {:style "display: flex; justify-content: center; align-items: center;"} "OR"]
+           ;[:div.social-buttons
+           ; [:button.button.is-fullwidth
+           ;  {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
+             ;[:span.icon
+             ; [:i.mdi.mdi-google]]
+             ;[:span "Log in with Google"]]
+            ;[:button.button.is-fullwidth
+            ; {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
+             ;[:span.icon
+             ; [:i.mdi.mdi-facebook]]
+             ;[:span "Log in with Facebook"]]
+            ;[:button.button.is-fullwidth
+            ; {:style "background-color: #fff; border-color: #dbdbdb; display: flex;"}
+             ;[:span.icon
+             ; [:i.mdi.mdi-apple]]
+             ;[:span "Log in with Apple"]]]
+           ;[:div.field-wrapper
+           ; [:input {:type "submit"
+           ;          :style "margin-top:-20px"
+                     ;}]]
+           ;[:span.signup
+           ; {:style "margin-top:5px"}
+            ;"Not a user?  Sign up"]]
+
+
+
+          ]]
         [:div.face.face-back
          [:div.content
           [:h2 "Forgot your password?"]
           [:small "Enter your email so we can send you a reset link for your password"]
-          [:form {:onsubmit "event.preventDefault()"}
+          [:form
            [:div.field-wrapper
-            [:input {:type "text" :name "email" :placeholder "email" :required "true"}]
+            [:input {:type "text" :name "email" :placeholder "email" :required true}]
             [:label "e-mail"]]
            [:div.field-wrapper
             [:input {:type "submit"}] "\">"]]]]
-        [:div.face.face-right
-         [:div.content
-          [:h2 "Sign up"]
-          [:form {:onsubmit "event.preventDefault()"}
-           [:div.field-wrapper {:style "margin-top: -1px"}
-            [:input {:type "text" :name "username" :placeholder "username" :required "true"}]
-            [:label "username"]]
-           [:div.field-wrapper
-            [:input {:type "text" :name "email" :placeholder "email" :required "true"}]
-            [:label "e-mail"]]
-           [:div.field-wrapper
-            [:input {:type "password" :name "password" :placeholder "password" :autocomplete "new-password" :pattern "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" :required "true" :title "Password must include at least 1 uppercase, 1 lowercase, 1 number, 1 special character,
-                            and be at least 8 characters long."}]
-            [:label "password"]]
-           [:div.field-wrapper
-            [:input {:type "password" :name "password2" :placeholder "password" :required "true"}]
-            [:label "re-enter password"]]
-           [:label.checkbox {:style "font-size:10px"}
-            [:input {:type "checkbox" :required "true"}] "I have read and agreed to the" [:a {:href "#"} "Terms and Conditions"]]
-           [:div.field-wrapper {:style "margin-top: 5px; margin-bottom: -15px"}
-            [:input {:type "submit"}]]
-           [:span.singin {:onclick "showLogin()"} "Already a user?  Sign in"]]]]
+        ;[:div.face.face-right
+        ; [:div.content
+        ;  [:h2 "Sign up"]
+        ;  [:form
+        ;   [:div.field-wrapper {:style "margin-top: -1px"}
+        ;    [:input {:type "text" :name "username" :placeholder "username" :required true}]
+        ;    [:label "username"]]
+        ;   [:div.field-wrapper
+        ;    [:input {:type "text" :name "email" :placeholder "email" :required true}]
+        ;    [:label "e-mail"]]
+        ;   [:div.field-wrapper
+        ;    [:input {:type         "password" :name "password"
+        ;             :placeholder  "password"
+        ;             :pattern      "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+        ;             :title "Password must include at least 1 uppercase, 1 lowercase, 1 number, 1 special character, and be at least 8 characters long."}]
+        ;    [:label "password"]]
+        ;   [:div.field-wrapper
+        ;    [:input {:type "password" :name "password2" :placeholder "password" :required true}]
+        ;    [:label "re-enter password"]]
+        ;   [:label.checkbox {:style "font-size:10px"}
+        ;    [:input {:type "checkbox" :required true}] "I have read and agreed to the" [:a {:href "#"} "Terms and Conditions"]]
+        ;   [:div.field-wrapper {:style "margin-top: 5px; margin-bottom: -15px"}
+        ;    [:input {:type "submit"}]]
+        ;   [:span.singin "Already a user?  Sign in"]]]]
+
         [:div.face.face-left
          [:div.content
           [:h2 "Contact us"]
-          [:form {:onsubmit "event.preventDefault()"}
+          [:form
            [:div.field-wrapper
-            [:input {:type "text" :name "name" :placeholder "name" :required "true"}]
+            [:input {:type "text" :name "name" :placeholder "name" :required true}]
             [:label "Name"]]
            [:div.field-wrapper
-            [:input {:type "text" :name "email" :placeholder "email" :required "true"}]
+            [:input {:type "text" :name "email" :placeholder "email" :required true}]
             [:label "e-mail"]]
            [:div.field-wrapper
-            [:textarea {:placeholder "your message" :required "true"}]
+            [:textarea {:placeholder "your message" :required true}]
             [:label "your message"]]
            [:div.field-wrapper
             [:input {:type "submit"}]]]]]
+
         [:div.face.face-bottom
          [:div.content
           [:div.thank-you-msg "Thank you!"]]]]]]
