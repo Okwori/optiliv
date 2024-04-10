9e58d66d69e69cf98ea62203dbb6b39448fef061
Simon Okwori <simon.okwori@gmail.com>
2024-04-10 04:10:22 -0500

    Modified register handler

--- src/clj/com/pringwa/optiliv/web/controllers/register.clj
+++ src/clj/com/pringwa/optiliv/web/controllers/register.clj
@@ -12,10 +12,10 @@
           {:strs [email full-name mobile user-group-id]} multipart-params
           account-type-id (Integer/parseInt user-group-id)
           _ (println "Here: " email full-name token mobile account-type-id)
-          ok? (query-fn :create-user! {:email  email :password nil :full_name full-name
+          _ (query-fn :create-user! {:email  email :password nil :full_name full-name
                                        :mobile mobile :token token :active false
                                        :account_type_id account-type-id :account_state_id 1})
-          _ (when ok? (email/welcome-customer send-fn email token))]
+          _ (email/welcome-customer send-fn email token)]
       {:status 201,
        :body   {}})
     (utils/error-response 403 "forbidden")))
