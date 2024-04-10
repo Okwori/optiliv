23800348caf42e92cd845d9bea5074d9237e66bb
Simon Okwori <simon.okwori@gmail.com>
2024-04-09 19:26:56 -0500

    Rmv placeholder text!

--- src/cljs/com/pringwa/optiliv/page/home.cljs
+++ src/cljs/com/pringwa/optiliv/page/home.cljs
@@ -84,38 +84,39 @@
               [:div.field
                [:div.control.custom-control
                 [:button.button.is-warning.is-rounded {:onclick "toggleFilters()"} "All Filters"]]]]]]]
-          [:div.columns
-           [:div.column
-            [:section.hero
-             [:div.hero-body
-              [:div.container
-               [:div#app.row.columns.is-multiline
-                [:div.column.is-5 {:v-for "card in cardData" ::key "card.id"}
-                 [:div.card.large
-                  [:div.card-image
-                   [:figure.image.is-16by9
-                    [:img {:src "card.image" :alt "Image"}]]]
-                  [:div.card-content
-                   [:div.media
-                    [:div.media-content
-                     [:p.title.is-5.has-text-weight-bold "{{card.user.price}}"]
-                     [:p.subtitle.is-6.is-inline.is-centered [:span.mdi.mdi-bed-queen] "{{card.user.beds}} |"]
-                     [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-shower] "{{card.user.baths}}  |"]
-                     [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-ruler-square] "{{card.user.dimensions}}"]
-                     [:p.subtitle.is-6 "{{card.user.address}}"]]]]]]]]]]]
-           [:div.column
-            [:section.hero
-             [:div.hero-body
-              [:div.container
-               [:div.card.large
-                [:div.media
-                 [:div.media-content
-                  [:div#googleMap
-                   [:script "function myMap() {
-                                                var mapProp= {
-                                                    center:new google.maps.LatLng(32.5252,-93.7502),
-                                                    zoom:12,
-                                                };
-                                                var map = new google.maps.Map(document.getElementById(\"googleMap\"),mapProp);
-                                            }"]
-                   [:script {:src "https://maps.googleapis.com/maps/api/js?key=AIzaSyCo9newQ_qNyc1HUK_LcpOtaQlUXTjc2co&callback=myMap"}]]]]]]]]]]]]]])))
+          ;[:div.columns
+          ; [:div.column
+          ;  [:section.hero
+          ;   [:div.hero-body
+          ;    [:div.container
+          ;     [:div#app.row.columns.is-multiline
+          ;      [:div.column.is-5 {:v-for "card in cardData" ::key "card.id"}
+          ;       [:div.card.large
+          ;        [:div.card-image
+          ;         [:figure.image.is-16by9
+          ;          [:img {:src "card.image" :alt "Image"}]]]
+          ;        [:div.card-content
+          ;         [:div.media
+          ;          [:div.media-content
+          ;           [:p.title.is-5.has-text-weight-bold "{{card.user.price}}"]
+          ;           [:p.subtitle.is-6.is-inline.is-centered [:span.mdi.mdi-bed-queen] "{{card.user.beds}} |"]
+          ;           [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-shower] "{{card.user.baths}}  |"]
+          ;           [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-ruler-square] "{{card.user.dimensions}}"]
+          ;           [:p.subtitle.is-6 "{{card.user.address}}"]]]]]]]]]]]
+          ; [:div.column
+          ;  [:section.hero
+          ;   [:div.hero-body
+          ;    [:div.container
+          ;     [:div.card.large
+          ;      [:div.media
+          ;       [:div.media-content
+          ;        [:div#googleMap
+          ;         [:script "function myMap() {
+          ;                                      var mapProp= {
+          ;                                          center:new google.maps.LatLng(32.5252,-93.7502),
+          ;                                          zoom:12,
+          ;                                      };
+          ;                                      var map = new google.maps.Map(document.getElementById(\"googleMap\"),mapProp);
+          ;                                  }"]
+          ;         [:script {:src "https://maps.googleapis.com/maps/api/js?key=AIzaSyCo9newQ_qNyc1HUK_LcpOtaQlUXTjc2co&callback=myMap"}]]]]]]]]]]
+          ]]]])))
