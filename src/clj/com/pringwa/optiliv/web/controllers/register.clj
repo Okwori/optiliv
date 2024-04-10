0784cfbae6fb23862b98e6ede37e1312a03df908
Simon Okwori <simon.okwori@gmail.com>
2024-04-10 10:08:19 -0500

    Latest deploy

--- src/clj/com/pringwa/optiliv/web/controllers/register.clj
+++ src/clj/com/pringwa/optiliv/web/controllers/register.clj
@@ -12,9 +12,10 @@
           {:strs [email full-name mobile user-group-id]} multipart-params
           account-type-id (Integer/parseInt user-group-id)
           _ (println "Here: " email full-name token mobile account-type-id)
-          _ (query-fn :create-user! {:email  email :password nil :full_name full-name
-                                       :mobile mobile :token token :active false :last_login nil
-                                       :account_type_id account-type-id :account_state_id 1})
+          _ (query-fn :create-user! {:email           email :password nil :full_name full-name
+                                     :mobile          mobile :token token :active false :last_login nil
+                                     :account_type_id account-type-id :account_state_id 1})
+          _ (println "GOT IT!!")
           _ (email/welcome-customer send-fn email token)]
       {:status 201,
        :body   {}})
