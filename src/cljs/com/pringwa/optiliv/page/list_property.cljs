89549a0b12d177679a826435b602ce99900ae5e6
Simon Okwori <simon.okwori@gmail.com>
2024-04-17 11:36:52 -0500

    Updated listings page

--- src/cljs/com/pringwa/optiliv/page/list_property.cljs
+++ src/cljs/com/pringwa/optiliv/page/list_property.cljs
@@ -25,4 +25,84 @@
           [:div.level
            [:div.level-left
             [:div.level-item.is-hero-content-item
-             [:div [:h1.title.is-spaced [:b "TODO"]]]]]]]]]])))
+             [:div [:h1.title.is-spaced [:b "List a Property"]]]]]]]]
+        [:section.section
+         [:div.container
+          [:form {:action "submit_listing.php" :method "post" :enctype "multipart/form-data"}
+           [:div.field
+            [:label.label {:for "address"} "Property Address:"]
+            [:div.control
+             [:input#address.input {:type "text" :name "address" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "price"} "Listing Price:"]
+            [:div.control
+             [:input#price.input {:type "text" :name "price" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "property_type"} "Property Type:"]
+            [:div.control
+             [:div.select
+              [:select#property_type {:name "property_type"}
+               [:option {:value "single_family"} "Single Family Home"]
+               [:option {:value "condo"} "Condo"]
+               [:option {:value "townhouse"} "Townhouse"]
+               [:option {:value "multi_family"} "Multi-family"]]]]]
+           [:div.field
+            [:label.label {:for "bedrooms"} "Number of Bedrooms:"]
+            [:div.control
+             [:input#bedrooms.input {:type "number" :name "bedrooms" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "bathrooms"} "Number of Bathrooms:"]
+            [:div.control
+             [:input#bathrooms.input {:type "number" :name "bathrooms" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "square_footage"} "Square Footage:"]
+            [:div.control
+             [:input#square_footage.input {:type "number" :name "square_footage" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "lot_size"} "Lot Size (if applicable):"]
+            [:div.control
+             [:input#lot_size.input {:type "text" :name "lot_size"}]]]
+           [:div.field
+            [:label.label {:for "year_built"} "Year Built:"]
+            [:div.control
+             [:input#year_built.input {:type "number" :name "year_built" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "description"} "Description of Property:"]
+            [:div.control
+             [:textarea#description.textarea {:name "description" :rows "4" :cols "50" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "contact_name"} "Agent/Owner Name:"]
+            [:div.control
+             [:input#contact_name.input {:type "text" :name "contact_name" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "agency"} "Agency (if applicable):"]
+            [:div.control
+             [:input#agency.input {:type "text" :name "agency"}]]]
+           [:div.field
+            [:label.label {:for "phone"} "Phone Number:"]
+            [:div.control
+             [:input#phone.input {:type "tel" :name "phone" :required "true"}]]]
+           [:div.field
+            [:label.label {:for "email"} "Email Address:"]
+            [:div.control
+             [:input#email.input {:type "email" :name "email" :required "true"}]]]
+           [:div.field
+            [:label.label "Preferred Method of Contact:"]
+            [:div.control
+             [:label.radio
+              [:input {:type "radio" :name "contact_method" :value "phone" :checked "true"}] "Phone"]
+             [:label.radio
+              [:input {:type "radio" :name "contact_method" :value "email"}] "Email"]
+             [:label.radio
+              [:input {:type "radio" :name "contact_method" :value "text"}] "Text"]]]
+           [:div.field
+            [:label.label {:for "additional_comments"} "Additional Comments or Information:"]
+            [:div.control
+             [:textarea#additional_comments.textarea {:name "additional_comments" :rows "4" :cols "50"}]]]
+           [:div.field
+            [:label.label {:for "photos"} "Upload Photos:"]
+            [:div.control
+             [:input#photos.input {:type "file" :name "photos[]" :multiple "true" :accept "image/*"}]]]
+           [:div.field
+            [:div.control
+             [:input.button.is-primary {:type "submit" :value "Submit"}]]]]]]]])))
