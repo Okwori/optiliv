d634cd17646426675fac7f5eedc85225a91ba058
Simon Okwori <simon.okwori@gmail.com>
2024-04-20 22:35:07 -0500

    Furniture insert migrations!

---
+++ resources/migrations/20240421033221-insert-into-furniture-table.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO furniture (id, name)
+VALUES (1, 'Furnished');
+--;;
+INSERT INTO furniture (id, name)
+VALUES (2, 'Partly Furnished');
+--;;
+INSERT INTO furniture (id, name)
+VALUES (3, 'Unfurnished');
