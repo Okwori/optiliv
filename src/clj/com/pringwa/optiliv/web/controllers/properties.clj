963893044ae0c4d02398f205dca4fdf8a8d5f4cd
Simon Okwori <simon.okwori@gmail.com>
2024-04-21 14:56:51 -0500

    Ppty handler fn refactor

--- src/clj/com/pringwa/optiliv/web/controllers/properties.clj
+++ src/clj/com/pringwa/optiliv/web/controllers/properties.clj
@@ -1,16 +1,23 @@
 (ns com.pringwa.optiliv.web.controllers.properties
   (:require
+    [com.pringwa.optiliv.web.model.map :as map]
     [com.pringwa.optiliv.web.routes.utils :as utils]))
+
+(defn query-map-api [req]
+  (let [{:keys [body-params]} req
+        {:keys [query-fn]} (utils/route-data req)
+        place-types (keep #(when (second %) (name (first %)))
+                          (:place-types body-params))
+        properties (query-fn :get-properties {})
+        result (if (empty? place-types)
+                 (map #(assoc % :ratings 0) properties)
+                 (pmap #(merge % (map/get-rating 400 (:address %) place-types))
+                       properties))]
+    result))
 
 (defn handler
   [{:keys [body-params session] :as req}]
-  (if session
-    (let [_ (println "PARAMS:" body-params)
-          place-types (keep #(when (second %) (name (first %)))
-                            (:place-type body-params))
-          result (if (not-empty place-types)
-                   nil
-                   nil)]
-      {:status 201,
-       :body   {}})
+  (if-let [result (query-map-api req)]
+    {:status 201,
+     :body   result}
     (utils/error-response 403 "forbidden")))
