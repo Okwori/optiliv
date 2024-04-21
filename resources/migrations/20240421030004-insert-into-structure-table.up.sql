4bc933a36a5b87669da58c695293ef56a946e8ca
Simon Okwori <simon.okwori@gmail.com>
2024-04-20 22:03:20 -0500

    Structure insert migration

---
+++ resources/migrations/20240421030004-insert-into-structure-table.up.sql
@@ -0,0 +1,17 @@
+INSERT INTO structure (id, name)
+VALUES (1, 'Studio');
+--;;
+INSERT INTO structure (id, name)
+VALUES (2, '1 Bedroom');
+--;;
+INSERT INTO structure (id, name)
+VALUES (3, '1.5 Bedroom');
+--;;
+INSERT INTO structure (id, name)
+VALUES (4, '2 Bedrooms');
+--;;
+INSERT INTO structure (id, name)
+VALUES (5, '2.5 Bedrooms');
+--;;
+INSERT INTO structure (id, name)
+VALUES (6, '3 Bedrooms');
