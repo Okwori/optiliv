a66904bf7ddba6d6e3e333e894ab1251c194138c
Simon Okwori <simon.okwori@gmail.com>
2024-04-21 11:45:53 -0500

    Updated addresses in migration scripts

--- resources/migrations/20240421081236-insert-into-property-table.up.sql
+++ resources/migrations/20240421081236-insert-into-property-table.up.sql
@@ -1,25 +1,25 @@
 insert into property
     (id, name, price, address, area, adjusted_by, user_id, description, image_url, city_area_id,
     property_type_id, structure_id, furniture_id)
-values (1, 'Diamond Apartments', 1200, '7000 Fern Ave, Shreveport, LA 71105', 80, 1, 1, '', 'img/apartment1.png', null, 1, 2, 1);
+values (1, 'Diamond Apartments', 1200, '7000 Fern Ave, Shreveport', 80, 1, 1, '', 'img/apartment1.png', null, 1, 2, 1);
 --;;
 insert into property
     (id, name, price, address, area, adjusted_by, user_id, description, image_url, city_area_id,
      property_type_id, structure_id, furniture_id)
-values (2, 'Golden Gate Apartments', 1100, '2600 Celebration Cove, Shreveport, LA 71105', 110, 1, 1, '', 'img/apartment2.png', null, 1, 2, 1);
+values (2, 'Golden Gate Apartments', 1100, '2600 Celebration Cove, Shreveport', 110, 1, 1, '', 'img/apartment2.png', null, 1, 2, 1);
 --;;
 insert into property
     (id, name, price, address, area, adjusted_by, user_id, description, image_url, city_area_id,
      property_type_id, structure_id, furniture_id)
-values (3, 'Garden Greens', 900, '1846 Fairfield Ave, Shreveport, LA 71101', 150, 1, 1, '', 'img/apartment3.png', null, 1, 2, 1);
+values (3, 'Garden Greens', 900, '1846 Fairfield Ave, Shreveport', 150, 1, 1, '', 'img/apartment3.png', null, 1, 2, 1);
 --;;
 insert into property
     (id, name, price, address, area, adjusted_by, user_id, description, image_url, city_area_id,
      property_type_id, structure_id, furniture_id)
-values (4, 'Purple Olives', 1600, '1620 Fullerton St, Shreveport, LA 7110', 80, 1, 1, '', 'img/apartment4.png', null, 1, 1, 1);
+values (4, 'Purple Olives', 1600, '1620 Fullerton St, Shreveport', 80, 1, 1, '', 'img/apartment4.png', null, 1, 1, 1);
 --;;
 insert into property
     (id, name, price, address, area, adjusted_by, user_id, description, image_url, city_area_id,
      property_type_id, structure_id, furniture_id)
-values (5, 'Sun City Estate', 1000, '113 Topango Dr, Shreveport, LA 71115', 80, 1, 1, '', 'img/apartment5.png', null, 1, 1, 1);
+values (5, 'Sun City Estate', 1000, '113 Topango Dr, Shreveport', 80, 1, 1, '', 'img/apartment5.png', null, 1, 1, 1);
 
