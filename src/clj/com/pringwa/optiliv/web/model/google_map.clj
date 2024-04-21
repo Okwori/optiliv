a5b88ff1ed5e1e63f636d0d5be27469ee5333d28
Simon Okwori <simon.okwori@gmail.com>
2024-04-20 21:41:18 -0500

    Google map helper ns

---
+++ src/clj/com/pringwa/optiliv/web/model/google_map.clj
@@ -0,0 +1,61 @@
+(ns com.pringwa.optiliv.web.model.google-map
+  (:require [clojure.walk :refer [keywordize-keys]])
+  (:import [com.google.maps GeoApiContext
+                            GeocodingApi
+                            DistanceMatrixApi]
+           [com.google.maps.model
+            LatLng]))
+
+(defn latlng [{:keys [lat lng]}]
+  (LatLng. lat lng))
+
+(defn address->coordinates [context p]
+  (let [r (first (. (GeocodingApi/geocode context p) await ))
+        n (->> (. r addressComponents)
+               (map #(hash-map (.toString (first (.types %))) (.longName %)))
+               (apply conj)
+               keywordize-keys)]
+    {:lat (-> r
+              .geometry
+              .location
+              .lat)
+     :lng (-> r
+              .geometry
+              .location
+              .lng)
+     :address (-> r .formattedAddress)
+     :city (:locality n)
+     :country (:country n)}))
+
+(defn coordinates->address [context latlng]
+  (let [r (first (. (GeocodingApi/reverseGeocode
+                      context
+                      latlng) await   ))
+        n (->> (. r addressComponents)
+               (map #(hash-map (.toString (first (.types %))) (.longName %)))
+               (apply conj)
+               keywordize-keys)]
+    {:address (-> r
+                  .formattedAddress)
+     :city (:locality n)
+     :country (:country n)}))
+
+(defn distance [context {:keys [a1 a2]}]
+  (let [r (. (DistanceMatrixApi/getDistanceMatrix
+               context
+               (into-array [a1])
+               (into-array [a2])) await)]
+    {:distance (-> r
+                   .rows
+                   first
+                   .elements
+                   first
+                   .distance
+                   .inMeters)
+     :duration (-> r
+                   .rows
+                   first
+                   .elements
+                   first
+                   .duration
+                   .inSeconds)}))
