4904cfe5f8e2eed4c96ac8ef0e36b21412436d7c
Simon Okwori <simon.okwori@gmail.com>
2024-04-20 21:38:31 -0500

    Ppty type migration

---
+++ resources/migrations/20240421023445-insert-property-type-table.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO property_type (id, name)
+VALUES (1, 'Apartment');
+--;;
+INSERT INTO property_type (id, name)
+VALUES (2, 'House');
+--;;
+INSERT INTO property_type (id, name)
+VALUES (3, 'Shop');
+--;;
+INSERT INTO property_type (id, name)
+VALUES (4, 'Office Space');
