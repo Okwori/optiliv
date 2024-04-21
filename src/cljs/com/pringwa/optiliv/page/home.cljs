1ac0f1bd00a346660b20587176e721e0891319ee
Simon Okwori <simon.okwori@gmail.com>
2024-04-21 14:53:56 -0500

    Improve home page

--- src/cljs/com/pringwa/optiliv/page/home.cljs
+++ src/cljs/com/pringwa/optiliv/page/home.cljs
@@ -10,12 +10,18 @@
     [clojure.string :as str]
     [re-frame.core :as rf]))
 
+(defn rating-color [rating]
+  (cond
+    (<= rating 30) "#dc3545"
+    (<= rating 60) "#ffc107"
+    :else "#28a745"))
 
 (defn home-page []
   (let [current-user-type @(rf/subscribe [::data/current-user-type])
         current-user-name (rf/subscribe [::data/current-user-name])
         current-user-name-coll (str/split (or @current-user-name "My Account") #" ")
-        active-slide-tab @(rf/subscribe [::data/active-slide-tab])]
+        active-slide-tab @(rf/subscribe [::data/active-slide-tab])
+        xhr-status (rf/subscribe [::data/xhr :properties])]
     (if-not (or (= "Optiliv" current-user-type)
                 (= "Agents" current-user-type)
                 (= "Customers" current-user-type))
@@ -37,8 +43,8 @@
               [:div.level-item.is-hero-content-item
                [:div [:h3.title.is-spaced [:b "Properties found"]]]]]]])]
 
-        [:section.section.is-main-section
-         (when (not active-slide-tab)
+        (when (not active-slide-tab)
+          [:section.section.is-main-section
            [:div.container
             [:div.columns.is-centered
              [:div.column
@@ -59,7 +65,8 @@
                    [:a
                     {:on-click #(rf/dispatch [::events/load-next-slide-and-properties])}
                     [:span "Next"]
-                    [:span.icon [:i.mdi.mdi-pan-right {:aria-hidden "true"}]]]]]]
+                    [:span.icon [:i.mdi.mdi-pan-right {:aria-hidden "true"}]]
+                    [xhrs/xhr-status :properties @xhr-status]]]]]
                 [:div.field
 
                  ;; Distance
@@ -526,100 +533,104 @@
                     [:div.control
                      [:label.checkbox
                       [:input {:type "checkbox"}]
-                      [:span] " North"]]]]]]]]]]])]
+                      [:span] " North"]]]]]]]]]]]])
         (when active-slide-tab
-          [:div.hero
-           [:div.hero-body
-            [:div.filer-bar
+          (let [found-properties (reverse (sort-by :rating @(rf/subscribe [::data/properties])))]
+            [:div.hero
+             [:div.hero-body
+              [:div.filer-bar
+               [:div.columns
+                [:div.custom-fields
+                 [:div.field.has-addons.has-addons-right
+                  [:div.control
+                   [:input.input.is-rounded {:type "text" :placeholder "City, Neighborhood, ZIP, Address"
+                                             :default-value "Shreveport"}]]
+                  [:p.control
+                   [:a.button.is-rounded [:span.mdi.mdi-magnify]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control
+                   [:div.select.is-rounded
+                    [:select [:option "For Sale"] [:option "For Rent"] [:option "Sold"]]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control
+                   [:div.select.is-rounded
+                    [:select [:option "Beds"] [:option {:value "1"} "1"] [:option {:value "2"} "2"]
+                     [:option {:value "3"} "3"] [:option {:value "4"} "4"] [:option {:value "5"} "5"]]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control
+                   [:div.select.is-rounded
+                    [:select
+                     [:option "Baths"]
+                     [:option {:value "1"} "1"]
+                     [:option {:value "2"} "2"]
+                     [:option {:value "3"} "3"]
+                     [:option {:value "4"} "4"]
+                     [:option {:value "5"} "5"]]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control
+                   [:div.select.is-rounded
+                    [:select
+                     [:option {:value "-1"} "Price"]
+                     [:option {:value "50,000"} "$50,000"]
+                     [:option {:value "100,000"} "$100,000"]
+                     [:option {:value "150,000"} "$150,000"]
+                     [:option {:value "200,000"} "$200,000"]
+                     [:option {:value "250,000"} "$250,000"]
+                     [:option {:value "300,000"} "$300,000"]]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control.custom-control
+                   [:div.select.is-rounded
+                    [:select
+                     [:option "Property Type"]
+                     [:option "House"]
+                     [:option "Apartment"]
+                     [:option "Condo"]
+                     [:option "Townhouse"]
+                     [:option "Lot/Land"]]]]]]
+                [:div.custom-fields
+                 [:div.field
+                  [:div.control.custom-control
+                   [:button.button.is-warning.is-rounded {:on-click "toggleFilters()"} "All Filters"]]]]]]]
              [:div.columns
-              [:div.custom-fields
-               [:div.field.has-addons.has-addons-right
-                [:div.control
-                 [:input.input.is-rounded {:type "text" :placeholder "City, Neighborhood, ZIP, Address"}]]
-                [:p.control
-                 [:a.button.is-rounded [:span.mdi.mdi-magnify]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control
-                 [:div.select.is-rounded
-                  [:select [:option "For Sale"] [:option "For Rent"] [:option "Sold"]]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control
-                 [:div.select.is-rounded
-                  [:select [:option "Beds"] [:option {:value "1"} "1"] [:option {:value "2"} "2"]
-                   [:option {:value "3"} "3"] [:option {:value "4"} "4"] [:option {:value "5"} "5"]]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control
-                 [:div.select.is-rounded
-                  [:select
-                   [:option "Baths"]
-                   [:option {:value "1"} "1"]
-                   [:option {:value "2"} "2"]
-                   [:option {:value "3"} "3"]
-                   [:option {:value "4"} "4"]
-                   [:option {:value "5"} "5"]]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control
-                 [:div.select.is-rounded
-                  [:select
-                   [:option {:value "-1"} "Price"]
-                   [:option {:value "50,000"} "$50,000"]
-                   [:option {:value "100,000"} "$100,000"]
-                   [:option {:value "150,000"} "$150,000"]
-                   [:option {:value "200,000"} "$200,000"]
-                   [:option {:value "250,000"} "$250,000"]
-                   [:option {:value "300,000"} "$300,000"]]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control.custom-control
-                 [:div.select.is-rounded
-                  [:select
-                   [:option "Property Type"]
-                   [:option "House"]
-                   [:option "Apartment"]
-                   [:option "Condo"]
-                   [:option "Townhouse"]
-                   [:option "Lot/Land"]]]]]]
-              [:div.custom-fields
-               [:div.field
-                [:div.control.custom-control
-                 [:button.button.is-warning.is-rounded {:on-click "toggleFilters()"} "All Filters"]]]]]]]
-           [:div.columns
-            [:div.column
-             [:section.hero
-              [:div.hero-body
-               [:div.container
-                [:div#app.row.columns.is-multiline
-                 [:div.column.is-5
+              [:div.column
+               [:section.hero
+                [:div.hero-body
+                 [:div.container
+                  [:div.row.columns.is-multiline
+                   (for [property found-properties]
+                     [:div.column.is-5
+                      [:div.card.large
+                       [:div.card-image
+                        [:figure.image.is-16by9
+                         [:img {:src (:image_url property) :alt (:name property)}]
+                         [:div.rating-circle {:style {:background-color (rating-color (:rating property))}} (str (:rating property))]]]
+                       [:div.card-content
+                        [:div.media
+                         [:div.media-content
+                          [:p.title.is-5.has-text-weight-bold (str (:name property) " $" (:price property))]
+                          [:p.subtitle.is-6.is-inline.is-centered [:span.mdi.mdi-bed-queen]]
+                          [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-shower]]
+                          [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-ruler-square]]
+                          [:p.subtitle.is-6 (:address property)]]]]]])]]]]]
+              [:div.column
+               [:section.hero
+                [:div.hero-body
+                 [:div.container
                   [:div.card.large
-                   [:div.card-image
-                    [:figure.image.is-16by9
-                     [:img {:src "card.image" :alt "Image"}]]]
-                   [:div.card-content
-                    [:div.media
-                     [:div.media-content
-                      [:p.title.is-5.has-text-weight-bold "{{card.user.price}}"]
-                      [:p.subtitle.is-6.is-inline.is-centered [:span.mdi.mdi-bed-queen] "{{card.user.beds}} |"]
-                      [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-shower] "{{card.user.baths}}  |"]
-                      [:p.subtitle.is-6.is-inline.centered-content [:span.mdi.mdi-ruler-square] "{{card.user.dimensions}}"]
-                      [:p.subtitle.is-6 "{{card.user.address}}"]]]]]]]]]]]
-            [:div.column
-             [:section.hero
-              [:div.hero-body
-               [:div.container
-                [:div.card.large
-                 [:div.media
-                  [:div.media-content
-                   [:div#googleMap
-                    [:script "function myMap() {
+                   [:div.media
+                    [:div.media-content
+                     [:div#googleMap
+                      [:script "function myMap() {
                                                  var mapProp= {
                                                      center:new google.maps.LatLng(32.5252,-93.7502),
                                                      zoom:12,
                                                  };
                                                  var map = new google.maps.Map(document.getElementById(\"googleMap\"),mapProp);
                                              }"]
-                    [:script {:src "https://maps.googleapis.com/maps/api/js?key=AIzaSyCo9newQ_qNyc1HUK_LcpOtaQlUXTjc2co&callback=myMap"}]]]]]]]]]]
-           ])]])))
+                      [:script {:src "https://maps.googleapis.com/maps/api/js?key=AIzaSyCo9newQ_qNyc1HUK_LcpOtaQlUXTjc2co&callback=myMap"}]]]]]]]]]]
+             ]))]])))
